d9fa0456e9ba767ed7d740ac39a79f0e1a296d35
Vinícius Seabra de Carvalho <vseabra@protonmail.com>
2025-01-25 10:20:24 -0300

    feat: implement is-slot-available-with-events

--- src/2-is-slot-available-with-events/is-slot-available-with-events.ts
+++ src/2-is-slot-available-with-events/is-slot-available-with-events.ts
@@ -1,10 +1,22 @@
+import { addMinutes } from 'date-fns';
 import { CalendarAvailability, CalendarEvent, CalendarSlot } from '../types';
+import { isSlotAvailable } from '../1-is-slot-available/is-slot-available';
 
 export const isSlotAvailableWithEvents = (
   availability: CalendarAvailability,
   events: Array<Omit<CalendarEvent, 'buffer'>>,
   slot: CalendarSlot,
 ): boolean => {
-  // Your code goes here
-  return false;
+  const { start, durationM } = slot;
+  const end = addMinutes(start, durationM)
+
+  const isAvailableBySchedule = isSlotAvailable(availability, slot);
+  if (!isAvailableBySchedule) {
+    return false;
+  }
+  const isOverlappingWithEvents = events.some(event => 
+    (start < event.end && end > event.start)
+  );
+
+  return !isOverlappingWithEvents;
 };
